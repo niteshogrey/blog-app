945a54add6c69bc5b6b8189e904b37e0c17c4c7c
Nitesh Ogrey <152773179+niteshogrey@users.noreply.github.com>
2025-01-23 12:20:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Allows access from network devices
-    port: 3000, // Set a custom port (default is 5173)
-    open: true, // Automatically opens the app in the default browser
-  },
 });
