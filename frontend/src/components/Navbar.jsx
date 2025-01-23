3d0bc8595da056612f3718a77c1c7eea74dd96af
Nitesh Ogrey <152773179+niteshogrey@users.noreply.github.com>
2025-01-23 12:01:00 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
       const fetchUsername = async () => {
         try {
           const { data } = await axios.get(
-            `https://blog-app-mx48.onrender.com/api/v1/user/user/${userId}`
+            `https://blog-app-1dc8.onrender.com/api/v1/user/user/${userId}`
           );
           setUsername(data.user?.username);
           // console.log(data.user?.username);
